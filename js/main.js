f02ab8841307b873f0b9e47c37f5357a2c251877
Khalil Howard <khalil.howard13@hotmail.com>
2021-12-13 23:12:45 -0600

    Randomize tile colors to represent safe vs unsafe

--- js/main.js
+++ js/main.js
@@ -1,12 +1,30 @@
+// constants
+
+let tileType = ['rgb(204,0,0)', 'rgb(0,204,0)'];
+let gameDifficulty = {
+    "easy": {totalBombs: 10},
+    "medium": {totalBombs: 25},
+    "hard": {totalBombs: 40}
+};
+
+//Event Listener for Tile click
 const elements = document.querySelectorAll(".box")
 console.log(elements)
-//elements.addEventListener("click", test())
-//Is this a safeTile or mineTile
-function test (event) {
-  console.log("click", event)
+
+function test (event) {  
+  console.log("click", event);
 }
 
 for(let i = 0; i < elements.length; i++){
     elements[i].addEventListener("click", test)
+    assignBombs()
+};
+
+//assign bombs 
+function assignBombs() {
+ for(let i = 0; i < elements.length; i++){
+      const randomBomb = Math.floor(Math.random()* tileType.length);       
+      elements[i].style.backgroundColor = tileType[randomBomb];
+ }
 }
 
